ae887fb09f4a60b0bd4ef8040fce05be882ffe69
anshi06 <araghya6768@gmail.com>
2022-09-09 13:08:31 +0530

    Styling Using CSS modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,42 +1,42 @@
-// import React from "react";
-import styled from "styled-components";
+import React from "react";
+//import styled from "styled-components";
 
-//import './Button.css';
-const Button = styled.button`
+import styles from './Button.module.css';
+// const Button = styled.button`
     
-    width : 100%;
-    font: inherit;
-    padding: 0.5rem 1.5rem;
-    border: 1px solid #8b005d;
-    color: white;
-    background: #8b005d;
-    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-    cursor: pointer;
-    @media (min-width: 786px){
-    width : auto;
-    }
+//     width : 100%;
+//     font: inherit;
+//     padding: 0.5rem 1.5rem;
+//     border: 1px solid #8b005d;
+//     color: white;
+//     background: #8b005d;
+//     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//     cursor: pointer;
+//     @media (min-width: 786px){
+//     width : auto;
+//     }
 
-  &:focus {
-    outline: none;
-  }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
-// Attacked Template literal
-// .button is a special kind of method in package, and this method will return a new button, there are so many methods for html elements
-//like div / h2 / p / etc.
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+// // Attacked Template literal
+// // .button is a special kind of method in package, and this method will return a new button, there are so many methods for html elements
+// //like div / h2 / p / etc.
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
